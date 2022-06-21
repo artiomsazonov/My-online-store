7441fb79133af68e584d6a352778bb7ded14c3e0
Artyom Sazonov <sazonau.artiom@yandex.by>
2022-06-21 15:46:23 +0300

    add device + descriptions

--- client/src/http/deviceAPI.js
+++ client/src/http/deviceAPI.js
@@ -26,19 +26,19 @@ export const createDevice = async (device) => {
     return data
 }
 
-// export const fetchDevices = async () => {
-//     const { data } = await $host.get("api/device")
-//     return data
-// }
-
-export const fetchDevices = async (typeId, brandId, page, limit = 5) => {
-    const { data } = await $host.get('api/device', {
-        params: {
-            typeId, brandId, page, limit
-        }
-    })
+export const fetchDevices = async () => {
+    const { data } = await $host.get("api/device")
     return data
 }
+
+// export const fetchDevices = async (typeId, brandId, page, limit = 5) => {
+//     const { data } = await $host.get('api/device', {
+//         params: {
+//             typeId, brandId, page, limit
+//         }
+//     })
+//     return data
+// }
 
 export const fetchOneDevice = async (id) => {
     const { data } = await $host.get('api/device/' + id)
